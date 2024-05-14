86a2df2197d94adc3b82612437f54b81a499d8ba
feroz-khann <ferozk2525@gmail.com>
2024-05-14 22:53:41 +0530

    added string reverse program

---
+++ strings.java
@@ -0,0 +1,8 @@
+public class strings {
+    public static void main(String[] args) {
+        StringBuilder str = new StringBuilder("Tony");
+
+        str.setCharAt(0,'s');
+        System.out.println(str);
+    }
+}
